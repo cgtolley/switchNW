bade9a98a761c66c0918445910ee61159a7892a7
cgtolley <charlieglenn.t@gmail.com>
2025-04-15 18:48:34 -0700

    silly fix

--- switch_network/switch_network.py
+++ switch_network/switch_network.py
@@ -12,14 +12,14 @@ gpios = [0,1,2]
 
 class SwitchNetwork:
 
-    def __init__(self, gpios=gpios, paths=paths, sernum = '/dev/ttyACM0'):
+    def __init__(self, gpios=gpios, paths=paths, serport= '/dev/ttyACM0'):
 
         '''make a dictionary of the objects and their switch paths.
         set up the software interfacing.
         '''
         self.paths = paths #will just need to write this by hand. 
         self.state = None #state will initially be in the low power mode, defined as the key to the path that is all zeros.
-        self.ser = serial.Serial(sernum, 115200)
+        self.ser = serial.Serial(serport, 115200)
         self.powerdown()
  
     def switch(self, pathname, verbose=False):
